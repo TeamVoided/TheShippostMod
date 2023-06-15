6437777dbb8e934202b3f9d9f791600d8a32bc87
theendercore <theendercore@gmail.com>
2023-06-15 18:42:17 +0300

    - id change

--- src/main/kotlin/org/teamvoided/shippost/TheShippostMod.kt
+++ src/main/kotlin/org/teamvoided/shippost/TheShippostMod.kt
@@ -3,18 +3,18 @@ package org.teamvoided.shippost
 import net.fabricmc.api.ModInitializer
 import net.minecraft.util.Identifier
 import org.slf4j.LoggerFactory
+import org.teamvoided.shippost.registries.ItemRegistry
 
 object TheShippostMod : ModInitializer {
     const val MODID = "shippost"
     val LOGGER = LoggerFactory.getLogger(MODID)
-
+    fun id(path: String): Identifier = Identifier(MODID, path)
     override fun onInitialize() {
         LOGGER.info("hi, Gun :gun:")
 
-       BoneInjection.init()
-
+        BoneInjection.init()
+        ItemRegistry.init()
     }
 
-    fun id(path: String): Identifier = Identifier(MODID, path)
-    
+
 }
