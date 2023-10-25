4f0d0361bad3f81d81f3e1b0b7714943fd59cb1d
theendercore <theendercore@gmail.com>
2023-10-26 01:03:42 +0300

    - Attempted advancement thing

---
+++ src/main/kotlin/org/teamvoided/shippost/gogoly_gagady/FatalStrike.kt
@@ -0,0 +1,27 @@
+package org.teamvoided.shippost.gogoly_gagady
+
+import net.fabricmc.fabric.api.entity.event.v1.ServerEntityCombatEvents
+import net.minecraft.advancement.Advancement
+import net.minecraft.advancement.AdvancementProgress
+import net.minecraft.server.network.ServerPlayerEntity
+import org.teamvoided.shippost.data.providers.AdvancementProvider.Companion.fatalStrike
+
+object FatalStrike {
+    fun inti() {
+        ServerEntityCombatEvents.AFTER_KILLED_OTHER_ENTITY.register{ server, killer, victim ->
+           if (killer is ServerPlayerEntity){
+//               println(killer.name)
+//               println(killer.uuid)
+               fatalStrike?.let { killer.grantAdvancement(it) }
+           }
+        }
+    }
+
+    private fun ServerPlayerEntity.grantAdvancement(advancement: Advancement){
+        val advancementProgress: AdvancementProgress = this.advancementTracker.getProgress(advancement)
+        if (advancementProgress.isDone) return
+
+        for (string in advancementProgress.unobtainedCriteria)
+            this.advancementTracker.grantCriterion(advancement, string)
+    }
+}
